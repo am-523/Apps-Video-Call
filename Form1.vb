7be6cb40601bab5b2c10e34b536ee83ae66408fd
Asril Mochammad <61135648+am-523@users.noreply.github.com>
2020-09-13 21:57:21 +0800

    Add files via upload

---
+++ Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Guna2Panel4_Paint(sender As Object, e As PaintEventArgs) Handles Guna2Panel4.Paint
+
+    End Sub
+
+    Private Sub Guna2Panel14_Paint(sender As Object, e As PaintEventArgs) Handles Guna2Panel14.Paint
+
+    End Sub
+End Class
